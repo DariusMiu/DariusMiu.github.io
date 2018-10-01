bb13cd4f12bd35833a5462d05d52a34b45747ec7
DariusMiu <darius@ln2.co>
2018-10-01 00:37:45 -0400

    test - graph may be done

--- test/grapher.js
+++ test/grapher.js
@@ -29,7 +29,7 @@ function Graph(cellData)
 	chartData[0] = [0, cellData.values[31][0], columns - 1, ['Concept Art', 'Animations', 'Patreon', 'Total Expenses', 'Net Expenses'], ['#38761D', '#4A86E8', '#FF5900', '#980000', '#9900FF'], ['','dashed','','','dashed'], ['#2D333A', [250, 500, 750, 1000]]]
 	// format: [min value, max value, number of columns, color of serieses] note: headers are at [x][0]
 	for (i = 1; i <= columns; i++)
-	{chartData[i] = [cellData.values[i][0], cellData.values[i][6], cellData.values[i][7], cellData.values[i][8], cellData.values[i][9], cellData.values[i][10]]; }
+	{ chartData[i] = [cellData.values[i][0], cellData.values[i][6], cellData.values[i][7], cellData.values[i][8], cellData.values[i][9], cellData.values[i][10]]; }
 	
 	var charts = document.getElementsByClassName('chart');
 	console.log('charts:' + charts.length + ' (expected: 1)');
@@ -112,8 +112,6 @@ function DrawGraph(chartDiv, data)
 	{
 		var div = document.createElement("div");
 		div.style.background = data[0][6][0];
-		div.style.left = 0;
-		div.style.width = '100%';
 		div.style.bottom = (100 * (data[0][6][1][i] / data[0][1])) + '%';
 		div.className = 'valueline';
 		var value = document.createElement("div");
@@ -123,6 +121,23 @@ function DrawGraph(chartDiv, data)
 		div.appendChild(value);
 		chartDiv.appendChild(div);
 	}
+	
+	for (i = 2; i < data.length; i++) // start at 2 to skip the first element cause it looks better
+	{
+		var div = document.createElement("div");
+		div.style.background = data[0][6][0];
+		div.style.left = (100 * (data[i][0] - data[1][0]) / daterange) + '%';
+		div.className = 'headerline';
+		var value = document.createElement("div");
+		value.className = 'header';
+		var date = new Date(epoch.valueOf());
+		date.setDate(epoch.getDate() + data[i][0]);
+		value.innerHTML = GetLongDate(date);
+		
+		div.appendChild(value);
+		chartDiv.appendChild(div);
+	}
+
 	
 	for (i = 1; i < data.length; i++)
 	{
